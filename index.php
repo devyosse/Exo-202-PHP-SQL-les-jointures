8ec46e35db2789d847497d84cda99e2874182845
devyosse <narutoshippuden884@gmail.com>
2022-02-25 11:57:43 +0100

    exo202

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+
+$server = 'localhost';
+$db = 'exo202';
+$user = 'root';
+$pass = '';
+
+// TODO Votre code ici.
+try {
+    $db = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pass);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
 /**
  * 1. Commencez par importer le script SQL disponible dans le dossier SQL.
  * 2. Connectez vous à la base de données blog.
@@ -15,17 +27,24 @@
  *
  * A l'aide d'une boucle, affichez chaque ligne du tableau de résultat.
  */
-
-// TODO Votre code ici.
-
+    $request = $db->prepare("SELECT article.id, article.title, article.content, categorie.name
+                               FROM article
+                               INNER JOIN categorie ON category_fk = categorie.id");
+    $request->execute();
+    echo "<pre>";  print_r($request->fetchAll());
+    echo "</pre>";
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
  */
 
 // TODO Votre code ici.
-
-
+    $request = $db->prepare("SELECT ar.id, ar.title, ar.content, ca.name
+                               FROM article as ar
+                               INNER JOIN categorie as ca ON category_fk = ca.id");
+    $request->execute();
+    echo "<pre>";  print_r($request->fetchAll());
+    echo "</pre>";
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
  *    Ajoutez des commentaires et liez un utilisateur au commentaire.
@@ -33,3 +52,19 @@
  */
 
 // TODO Votre code ici.
+    $sql ="INSERT INTO utilisateur (firstName, lastName, mail, password)
+           VALUES ('firstname', 'lastname', 'monmail@moi.pt', '1234azer')
+           ";
+
+    $result = $db->exec($sql);
+    $request = $db->prepare("SELECT auteur.firstName, auteur.lastName, article.title
+                                   FROM auteur
+                                   LEFT JOIN article ON auteur.id = article.author_fk");
+    $request->execute();
+    echo "<pre>";  print_r($request->fetchAll());
+    echo "</pre>";
+
+}
+catch (PDOException $e){
+    die($e->getMessage());
+}
